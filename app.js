8f6eab89aa21a55b76017cb01974951f10df8bcc
Takashy 54 <iversondavidmenez007@gmail.com>
2024-04-10 23:32:59 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className='App'>
+            <form action="">
+                <input type='text'></input>
+                <input type='text'></input>
+            </form>
+            <div className="test">
+                <h1>Hello</h1>
+            </div>
+        </div>
+    );
+}
+
+export default App; 
